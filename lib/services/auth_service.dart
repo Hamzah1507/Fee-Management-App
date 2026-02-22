e09018d3dd2e2a272134f98c801f1b890de992a4
Hamzah Saiyed <hamzah.2004saiyed@gmail.com>
2026-02-22 14:46:21 +0530

    chore: auto sync

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -31,7 +31,7 @@ class AuthService {
         'uid': user.uid,
         'name': name,
         'email': email,
-        'role': 'admin',
+        'role': 'Admin',
         'createdAt': FieldValue.serverTimestamp(),
       });
 
